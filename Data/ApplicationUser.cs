a04e84791b83f2fd858f14322c37a6d3a3d3f196
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2024-01-22 11:01:52 +0330

    Add ApplicationUser Table

---
+++ Data/ApplicationUser.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BookStore_WebApi.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
+
